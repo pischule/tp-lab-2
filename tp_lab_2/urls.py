500130371523f785ee7d03e0b2d9c974cbf88896
Maksim Pischulenok <pischule@outlook.com>
2021-03-22 15:46:05 +0300

    add views

--- tp_lab_2/urls.py
+++ tp_lab_2/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
+    path('store/', include('store.urls')),
     path('', include('polls.urls')),
 ]
