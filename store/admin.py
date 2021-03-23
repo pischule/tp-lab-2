113bdb2f549ac5c6ab1dc68dd51b9b564f15808b
Maksim Pischulenok <pischule@outlook.com>
2021-03-24 01:10:35 +0300

    add Order to admin

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Product, User
+from .models import Product, User, Order
 
 admin.site.register(Product)
+admin.site.register(Order)
 admin.site.register(User)
