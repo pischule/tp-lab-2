ceceeebc3892e19f04a1bc2687182cb3d6bc0966
Maksim Pischulenok <pischule@outlook.com>
2021-03-31 00:42:05 +0300

    fix heroku 4

--- tp_lab_2/settings.py
+++ tp_lab_2/settings.py
@@ -129,15 +129,3 @@ AUTH_USER_MODEL = "accounts.User"
 LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
